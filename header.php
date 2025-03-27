4004555575374a02fa1da1f02b49ac85df12e41f
nnqtrung <nqtrung123tr@gmail.com>
2025-03-27 10:47:47 +0700

    fix header

--- header.php
+++ header.php
@@ -1,24 +1,23 @@
 <header>
-    <div class="navbar">
-      <div class="navbar-left">
-        <div class="logo">LuckyJob</div>
-        <input type="checkbox" id="menu-toggle" />
-        <nav class="navbar-menu">
-          <ul>
-            <li><a href="index.php" class="active">Home</a></li>
-            <li><a href="jobs.php">Jobs</a></li>
-            <li><a href="apply.php">Apply</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="enhancements.php">Enhancements</a></li>
-            <!-- An email link as required -->
-            <li><a href="105313596@student.swin.edu.au">Email</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div class="navbar-right">
-        <div class="location">Hanoi, VN</div>
-        <img src="./styles/images/avatar.png" class="user-avatar" alt="avatar">
-        <label for="menu-toggle" class="menu-btn">&#9776; Menu</label>
-      </div>
+  <div class="navbar">
+    <div class="navbar-left">
+      <div class="logo">LuckyJob</div>
+      <input type="checkbox" id="menu-toggle" />
+      <nav class="navbar-menu">
+        <ul>
+          <li><a href="index.php" class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>">Home</a></li>
+          <li><a href="jobs.php" class="<?= basename($_SERVER['PHP_SELF']) == 'jobs.php' ? 'active' : '' ?>">Jobs</a></li>
+          <li><a href="apply.php" class="<?= basename($_SERVER['PHP_SELF']) == 'apply.php' ? 'active' : '' ?>">Apply</a></li>
+          <li><a href="about.php" class="<?= basename($_SERVER['PHP_SELF']) == 'about.php' ? 'active' : '' ?>">About</a></li>
+          <li><a href="enhancements.php" class="<?= basename($_SERVER['PHP_SELF']) == 'enhancements.php' ? 'active' : '' ?>">Enhancements</a></li>
+          <li><a href="mailto:105313596@student.swin.edu.au">Email</a></li> <!-- Fixed email link -->
+        </ul>
+      </nav>
     </div>
-  </header>
+    <div class="navbar-right">
+      <div class="location">Hanoi, VN</div>
+      <img src="./styles/images/avatar.png" class="user-avatar" alt="User Avatar">
+      <label for="menu-toggle" class="menu-btn">&#9776; Menu</label>
+    </div>
+  </div>
+</header>
